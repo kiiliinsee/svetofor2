fbf4def96fa3320a7e5f10fb978549501de5b21e
kiiliinsee <konstantinova1111konstantinova@yandex.ru>
2025-04-23 01:32:36 +0300

    vs

---
+++ app/src/main/java/com/example/svetofor/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.svetofor
+
+
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var redLight: View
+    private lateinit var yellowLight: View
+    private lateinit var greenLight: View
+    private lateinit var nextButton: Button
+
+    private var currentStateIndex = 0
+    private val states = listOf("RED", "YELLOW1", "GREEN", "YELLOW2")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        redLight = findViewById(R.id.red_light)
+        yellowLight = findViewById(R.id.yellow_light)
+        greenLight = findViewById(R.id.green_light)
+        nextButton = findViewById(R.id.next_button)
+
+        if (savedInstanceState != null) {
+            currentStateIndex = savedInstanceState.getInt("STATE_INDEX", 0)
+        }
+
+        updateLights()
+
+        nextButton.setOnClickListener {
+            currentStateIndex = (currentStateIndex + 1) % states.size
+            updateLights()
+        }
+    }
+
+    private fun updateLights() {
+        redLight.setBackgroundColor(Color.GRAY)
+        yellowLight.setBackgroundColor(Color.GRAY)
+        greenLight.setBackgroundColor(Color.GRAY)
+
+        when (states[currentStateIndex]) {
+            "RED" -> redLight.setBackgroundColor(Color.RED)
+            "YELLOW1", "YELLOW2" -> yellowLight.setBackgroundColor(Color.YELLOW)
+            "GREEN" -> greenLight.setBackgroundColor(Color.GREEN)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("STATE_INDEX", currentStateIndex)
+    }
+}
+
